432ce8bb34093306ca92942c4f41a31338823a66
rimoldi98 <35239932+rimoldi98@users.noreply.github.com>
2020-10-01 23:54:34 -0300

    Added files.

---
+++ Assets/Scripts/LanguageDependentText.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class LanguageDependentText : MonoBehaviour
+{
+    private Text _text;
+
+    private void Start()
+    {
+        _text = GetComponent<Text>();
+
+        _text.text = LanguageLocalizer.GetTranslation(_text.text);
+    }
+}
---
+++ Assets/Scripts/LanguageDependentTextTMPro.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class LanguageDependentTextTMPro : MonoBehaviour
+{
+    private TextMeshProUGUI _text;
+
+    private void Start()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+
+        _text.text = LanguageLocalizer.GetTranslation(_text.text);
+    }
+}
